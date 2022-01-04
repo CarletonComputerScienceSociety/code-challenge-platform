1cac1350e3f68b3a4d0c9ac21db478fb33028efe
MathyouMB <matthewmacraebovell@bell.net>
2022-01-04 17:23:05 -0500

    add client template

---
+++ client/components/QuestionCard/QuestionCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { exampleStyle } from './QuestionCard.style';
+
+const QuestionCard = () => {
+  return (
+    <div style={exampleStyle}>Card</div>
+  )
+};
+
+export { QuestionCard };
---
+++ client/pages/test.jsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import { QuestionCard } from '../components'
+
+export default function Test() {
+  return (
+    <div className="container">
+        <QuestionCard/>
+    </div>
+  )
+}
